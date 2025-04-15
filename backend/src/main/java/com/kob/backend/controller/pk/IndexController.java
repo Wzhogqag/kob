960ceef91747da07496354fe84f5218b4d7ffe9a
wzq <2789170554@qq.com>
2025-04-15 21:00:37 +0800

    前后端链接测试完毕，跨域问题解决

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -2,19 +2,26 @@ package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 功能：
  * 作者： 程序员魏sir
  * 日期： 2025/4/15 16:21
  **/
-@Controller
+@RestController
 @RequestMapping("/pk/")
 public class IndexController {
 
-    @RequestMapping("index/")
-    public String index()
-    {
-        return "pk/index";
+    @RequestMapping("getbotinfo/")
+    public Map<String,String> getbotinfo() {
+        Map<String,String>bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","1500");
+
+        return bot1;
     }
 }
