a973a4978ceffe4687031fdd50f3f3311e133fc5
wzq <2789170554@qq.com>
2025-04-23 19:09:01 +0800

    --bug=1001812 --user=魏忠强 后端service层传值错误，已debug

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,7 +11,7 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import $ from 'jquery'
+// import $ from 'jquery'
 
 
 export default{
@@ -19,54 +19,50 @@ export default{
     NavBar
   },
   setup(){
-    $.ajax({
-      url: "http://localhost:3000/user/account/token/",
-      type:"post",
-      data:{
-        username:'wzq',
-        password:'pwzq',
-      },
-      success(resp){
-        console.log(resp);
-      },
-      error(resp){
-        console.log(resp);
-      },
-    });
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/token/",
+    //   type:"post",
+    //   data:{
+    //     username:'wzq',
+    //     password:'pwzq',
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   },
+    // });
 
-    $.ajax({
-      url: "http://localhost:3000/user/account/info/",
-      type:"get",
-      headers:{
-        Authorization:"Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzNjNhMTE3YWYzMmU0YWUxOGZlYzVjNzZiNDlhNDIyOCIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTc0NTMyNjI1NCwiZXhwIjoxNzQ2NTM1ODU0fQ.owhYAC5ml6N0dgZW05d6rrOmrSA6xv6ZqcvMV_kiukU"
-      },
-      success(resp){
-        console.log(resp);
-      },
-      error(resp){
-        console.log(resp);
-      },
-    });
-    $.ajax({
-      url: "http://localhost:3000/user/account/register/",
-      type:"post",
-      data:{
-        username:' ',
-        password:'pxt',
-        confirmedPassword:"pxt",
-      },
-      success(resp){
-        console.log(resp);
-      },
-      error(resp){
-        console.log(resp);
-      },
-    });
-
-
-
-
-  }
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/info/",
+    //   type:"get",
+    //   headers:{
+    //     Authorization:"Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzNjNhMTE3YWYzMmU0YWUxOGZlYzVjNzZiNDlhNDIyOCIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTc0NTMyNjI1NCwiZXhwIjoxNzQ2NTM1ODU0fQ.owhYAC5ml6N0dgZW05d6rrOmrSA6xv6ZqcvMV_kiukU"
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   },
+    // });
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/register/",
+    //   type:"post",
+    //   data:{
+    //     username:' ',
+    //     password:'pxt',
+    //     confirmedPassword:"pxt",
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   },
+    // });
+    }
 
 
 }
