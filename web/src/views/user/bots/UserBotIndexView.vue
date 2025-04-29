cc861f8fc34ec4fc0dd6e252474d76fa9599f66c
“yang” <411270093@qq.com>
2025-04-29 16:54:22 +0800

    --user=杨冬梅 bot功能跨域连接出错

--- web/src/views/user/bots/UserBotIndexView.vue
+++ web/src/views/user/bots/UserBotIndexView.vue
@@ -6,10 +6,32 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from "jquery"
 
 export default{
   components:{
     ContentField
+  },
+  setup() {
+    const jwt_token = localStorage.getItem("jwt_token");
+    $.ajax({
+      url: "http://localhost:3000/user/bot/add/",
+      type: "post",
+      data: {
+        title: "Bot的标题",
+        description: "Bot的描述",
+        content: "Bot的代码",
+      },
+      hesders: {
+        Authorization: "Bearer " + jwt_token,
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      },
+    });
   }
 
 }
